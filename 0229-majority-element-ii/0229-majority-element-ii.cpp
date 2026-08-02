3913fc107c6dee5f27a4b69262c93e927856b69d
Shubh <rshubh770@gmail.com>
2026-08-02 19:07:43 +0530

    Time: 2510 ms (5.17%), Space: 23.7 MB (69.05%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int, int>mpp;
-        int n = nums.size();
-        for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-        }
-        int cnt = n/3;
-        vector<int> v;
-        for(auto it:mpp){
-            if(it.second > cnt){
-                v.push_back(it.first);
+        // map<int, int>mpp;
+        // int n = nums.size();
+        // for(int i=0; i<nums.size(); i++){
+        //     mpp[nums[i]]++;
+        // }
+        // int cnt = n/3;
+        // vector<int> v;
+        // for(auto it:mpp){
+        //     if(it.second > cnt){
+        //         v.push_back(it.first);
+        //     }
+        // }
+        // return v;
+
+        vector<int>v;
+        for(int i = 0; i<nums.size(); i++){
+            int cnt = 0;
+            for(int j = 0; j<nums.size(); j++){
+                if(nums[i] == nums[j])cnt++;
+            } 
+            if(cnt> nums.size()/3){
+                if(v.size()==0 || v.back()!=nums[i]){
+                    v.push_back(nums[i]);
+                }
             }
+            if (v.size() == 2) {
+                if(v[0] > v[1])
+                    swap(v[0], v[1]);
+            break;
+        }
         }
         return v;
     }
