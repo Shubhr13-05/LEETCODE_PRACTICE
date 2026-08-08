728537052eddc572248423ba54ce567b145e6bc2
Shubh <rshubh770@gmail.com>
2026-08-08 10:13:45 +0530

    Time: 45 ms (12.53%) | Memory: 25.5 MB (8.16%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        set<char>st;
+        for(auto it:s){
+            st.insert(it);
+        }
+        return st.size();
+    }
+};
